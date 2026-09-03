7fdf665c857ae0f94f818cdc5c742d73e30d4196
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-09-03 23:42:39 +0530

    new components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
 
   return (
     <>
-      <BrowerRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowerRouter>
+      </BrowserRouter>
     </>
   )
 }
