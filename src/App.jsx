ba313cf8ca6f6b117b7a9927641dab0cde7a7bcc
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-09-03 23:30:06 +0530

    new components added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <BrowerRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowerRouter>
     </>
